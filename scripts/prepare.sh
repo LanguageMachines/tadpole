aff203a64734818a349f09f7dd0d69b869c5bde2
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2008-12-08 16:39:16 +0000

    removed
    git-svn-id: https://svn.uvt.nl/ilk/trunk/sources/Tadpole@2402 12f355fe-0486-481a-ad91-c297ab22b4e3

--- scripts/prepare.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/csh -f
-python=python
-rm -rf tadpole.ana.inst.*
-$python pairs.py -m20 --feats-bigram  tadpole.ana > tadpole.ana.inst
-$python dir.py tadpole.ana > tadpole.ana.dir.inst
-$python rels.py tadpole.ana > tadpole.ana.rels.inst
